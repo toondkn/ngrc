04148f68285d6ecba95927fb0e243e339440287a
Toon Derkoningen <toonderkoningen@gmail.com>
2020-05-21 20:59:05 +0200

    simplify ngrc

--- ngrc.js
+++ ngrc.js
@@ -1,69 +1,113 @@
 import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
-import { camelCaseIt, kebabCaseIt } from 'case-it'
 
-// A function that converts an angularjs scope object into a props object for a react component.
-function scopeToProps(scope, funcWrapper) {
+// functions to add or remove a str prefix (handles it camelCased)
+function addCcPrefix(prefix, str) {
+    return prefix + str.charAt(0).toUpperCase() + str.slice(1)
+}
+function removeCcPrefix(prefix, str) {
+    const withoutPrefix = str.slice(prefix.length)
+    return withoutPrefix.charAt(0).toLowerCase() + withoutPrefix.slice(1)
+}
+
+function ngrc(Component, bindPrefix = 'p') {
+    // return early if Component argument is unsatisfactory,
+    if (!Component) {
+        console.warn('ngrc: Did you forget to pass a react component?')
+        return {}
+    }
+    if (Component.ngrcBinds && !Array.isArray(Component.ngrcBinds)) {
+        console.warn('ngrc: [Component].ngrcBinds must be an array!')
+        return {}
+    }
+    if (!bindPrefix) {
+        console.warn('ngrc: Did you pass in a bind-prefix (nrgc() 2nd arg) that isn\'t a string of at least length 1?')
+        return {}
+    }
+
+    let binds
+    // set binds from [Component].ngrcBinds,
+    if (Array.isArray(Component.ngrcBinds)) {
+        binds = Component.ngrcBinds
+    }
+    // or infer them from Component's propTypes definition
+    if (!binds && typeof Component.propTypes === 'object') {
+        binds = Object.keys(Component.propTypes)
+    }
+
+    // create angularjs bindings object
+    const bindings = {}
+    for (const bind of (binds || [])) {
+        // we keep it simple: one-way binding only! why?:
+        // - disallows use of boilerplatey syntax due to angularjs func arg mapping obj
+        // - makes te implementation of this lib way faster/cleaner
+        // - '<' works for regular variables as well as functions
+        bindings[addCcPrefix(bindPrefix, bind)] = '<'
+    }
+
+    // return angularjs controller definition object with a dash of react interop
+    return {
+        bindings,
+        controller: ['$element', '$rootScope', function ($element, $rootScope) {
+            // NOTE: angularjs passes in the component scope into `this`
+            // it's the reason why the controller function is a regular anon func
+            // instead of an arrow function
+            // it's also the reason why we _have_ to define the below function in this scope,
+            // because it uses `this` internally!...
+
+            // a function that returns a function,
+            // with a rootScope.apply(), which wraps the passed in function
+            function wrapWithApply(func) {
+                return (...args) => {
+                    func(...args)
+                    // trigger an angularjs "re-render"
+                    $rootScope.$apply()
+                }
+            }
+
+            // get root component of angularjs component
+            const mountEl = $element[0]
+
+            // map angularjs <-> react lifecycles
+            // $onChanges is called before $onInit..., which is great for us:
+            // it means we only have to define $onChanges!
+            this.$onChanges = () => {
+                // react will just re-render the component if it is already initialized,
+                // perfect for our use case!
+                render(
+                    React.createElement(
+                        Component,
+                        scopeToProps(this, bindPrefix, bindings, wrapWithApply),
+                    ),
+                    mountEl
+                )
+            }
+            this.$onDestroy = () => {
+                // clean up react component
+                unmountComponentAtNode(mountEl)
+            }
+        }],
+    }
+}
+
+// massage angularjs scope object into a props object:
+// - only include bound scope properties
+// - wrap functions with passed in wrapFunc
+function scopeToProps(scope, bindPrefix, bindings, wrapFunc) {
     const props = {}
     for (const [key, value] of Object.entries(scope)) {
-        const propKey = camelCaseIt(key)
-        if (typeof value === 'function') {
-            props[propKey] = funcWrapper(value)
-        }
-        else {
-            props[propKey] = value
+        if (bindings[key]) {
+            const propName = removeCcPrefix(bindPrefix, key)
+            if (typeof value === 'function') {
+                props[propName] = wrapFunc(value)
+            }
+            else {
+                props[propName] = value
+            }
         }
     }
     return props
 }
 
 
-export default function (Component, propAttrPrefix) {
-    // We don't just use generate bindings for any attribute,
-    // because we may want to set some arbitrary attributes on the wrapper element itself.
-    // Be they regular HTML attributes, or angularjs specific ones.
-    // So we simply ask for a prefix.
-    const bindings = {}
-    // Use a react component's ngBindings definition to generate 
-    if (typeof Component.ngBindings === 'object') {
-        for (const [propName, bindingType] of Object.entries(Component.ngBindings)) {
-            const propNameAttr = kebabCaseIt(propName)
-            // Angularjs expects bindings to be defined in camelCase,
-            // even though you pass them with kebab-cased attributes on the component element.
-            bindings[camelCaseIt(`${propAttrPrefix}${propNameAttr}`)] = bindingType
-        }
-    }
-
-    return {
-        bindings,
-        // We use this kind of controller definition because it doesn't break after minification.
-        controller: ['$element', '$attrs', '$rootScope', ($element, $attrs, $rootScope) => {
-            console.log($element)
-            console.log($attrs)
-
-            // Since the functions passed to a react component can update a scope variable,
-            // we need to wrap the function to trigger a digest cycle after it executes.
-            function wrapFuncWithDigest(callback) {
-                return (...args) => {
-                    callback(...args)
-                    $rootScope.$apply()
-                }
-            }
-
-            // Note: angular sets 'this' to the '$scope'
-            const scope = this
-
-            // Handle angularjs <-> react component lifecycle syncing.
-            const mountEl = $element[0]
-            this.$onInit = () => {
-                render(React.createElement(Component, scopeToProps(scope, wrapFuncWithDigest), mountEl))
-            }
-            this.$onChanges = () => {
-                render(React.createElement(Component, scopeToProps(scope, wrapFuncWithDigest), mountEl))
-            }
-            this.$onDestroy = () => {
-                unmountComponentAtNode(mountEl)
-            }
-        }],
-    }
-}
+export default ngrc
